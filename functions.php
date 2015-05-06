8a191b468424877f69c35b99151e30af921dbf21
markdavid <markdpfaff@gmail.com>
2015-05-05 21:06:36 -0700

    This adds the start of my theme to wordpress

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+Theme Name: Mark Pfaff's WEB170 Theme
+Theme URI: http://markpfaff.com
+Author: Mark Pfaff
+Author URI: http://markpfaff.com
+Description: This theme was created for WEB170 at Seattle Central College
+License: GNU General Public License v2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Tags: 
+
+This theme, like WordPress, is licensed under the GPL.
+Use it to make something cool, have fun, and share what you've learned with others.
+*/
+
+//Register Menus
+
+register_nav_menus(array(
+    'main-menu'=> __('Main'),
+));
+
+?>
